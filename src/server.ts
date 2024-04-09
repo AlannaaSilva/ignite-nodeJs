057671a398cd4b5e00f5e12f32e7d61b8a7abae3
Alanna Silva <alannacostas55@gmail.com>
2024-04-09 17:43:02 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 
 import { env } from './env'
-import {app} from './app'
+import { app } from './app'
 
 app.listen({
   port: env.PORT,
+  host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
 }).then(() => {
   console.log("http server Running")
 })
